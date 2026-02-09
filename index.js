3c125906fb2d86bf847485e8a4d6bbdb45074a17
aboutitsankur <about.itsankur@gmail.com>
2026-02-09 12:10:15 +0530

    added new

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const fs =  require('fs');
-// fs.writeFile("./ad.txt","Coding is fun",()=>{});
-fs.readFile("./ad.txt","utf-8",(err,result)=>{
-    if(err){
-        console.log("Error",err);
-    }
-    else{
-        console.log(result);
-    }
-})
+// // fs.writeFile("./ad.txt","Coding is fun",()=>{});
+// fs.readFile("./ad.txt","utf-8",(err,result)=>{
+//     if(err){
+//         console.log("Error",err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// })
+
+// fs.appendFileSync("./ad.txt","done without callback");
+// fs.appendFile("./ad.txt","done with callback",()=>{});
+
+
+// fs.cpSync("a1.txt","b1.txt");
+// fs.cp("a1.txt","b1.txt",()=>{});
+
